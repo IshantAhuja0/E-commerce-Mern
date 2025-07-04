274828bf09f8e21197e74252d3141e8b2997f833
IshantAhuja0 <ishantahuja2309@gmail.com>
2025-07-04 11:20:08 +0530

    product bug fixed

--- Backend/models/product.model.js
+++ Backend/models/product.model.js
@@ -1,63 +1,62 @@
-import mongoose,{Schema} from "mongoose"
-function arrayLimit(val) {
-  return val.length>0
-}
-const productSchema=new Schema({
-  categoryId:{
-    type:Schema.Types.ObjectId,
-    ref:"Category",
-    required:true,
+import mongoose, { Schema } from "mongoose"
+const productSchema = new Schema({
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
   },
-  title:{
-    type:String,
-    trim:true,
-    maxlength:100,
-    required:true,
+  title: {
+    type: String,
+    trim: true,
+    maxlength: 100,
+    required: true,
   },
-  description:{
-    type:String,
-    trim:true,
-    maxlength:1000,
-    required:true,
+  description: {
+    type: String,
+    trim: true,
+    maxlength: 1000,
+    required: true,
   },
-  price:{
-    type:Number,
-    required:true,
-    default:0,
-    min:0
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+    min: 0
   },
-  stock:{
-    type:Number,
-    required:true,
-    default:0,
-    min:0
+  stock: {
+    type: Number,
+    required: true,
+    default: 0,
+    min: 0
   },
-  owner:{
-    type:Schema.Types.ObjectId,
-    ref:"User",
-    required:true,
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
   },
-  images:{
-    type:[{
-    url:{
-      type:String,
-      required:true,
-    },
-    public_id:{
-      type:String,
-      required:true,
-    },
-  }],
-validate:{
-  validator:[arrayLimit],
-  message:'At least one image is required'
-}
-}
-},{timestamps:true})
+  images: {
+    type: [{
+      url: {
+        type: String,
+        required: true,
+      },
+      public_id: {
+        type: String,
+        required: true,
+      },
+    }],
+    validate: {
+      validator: function (val) {
+        return val.length > 0
+      },
+      message: 'At least one image is required'
+    }
+  }
+}, { timestamps: true })
 
-productSchema.methods.isAvailable=async function(num) {
-  if(this.stock<num)return false
+productSchema.methods.isAvailable = async function (num) {
+  if (this.stock < num) return false
   else return true
 }
 
-export const Product=mongoose.model("Product",productSchema)
+export const Product = mongoose.model("Product", productSchema)
--- Backend/routes/product.routes.js
+++ Backend/routes/product.routes.js
@@ -1,7 +1,9 @@
 import  {Router} from "express"
 import { getProduct,addProduct, uploadImages, deleteProduct, deleteImages, updateProduct } from "../controllers/product.controller.js"
 import { upload } from "../middlewares/multer.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
 const router=Router()
+router.use(verifyJWT)
 router.route("/getproduct/:productId").get(getProduct)
 router.route("/addproduct").post(upload.array("images", 5), addProduct); // max 5 images
 router.route("/deleteproduct").post(deleteProduct); 
